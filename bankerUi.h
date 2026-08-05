0be32bf4ced3f0191dddf41b32baf4e670b084b6
embedded-engg-91 <badigermanoj781@gmail.com>
2026-08-05 18:41:13 +0530

    Update bankerUi.h

--- bankerUi.h
+++ bankerUi.h
@@ -9,14 +9,14 @@
 
 class BankerUI : public UI
 {
-    shared_ptr<Banker> banker; //Inside every BankerUI object, there is a pointer to a Banker object.
-    map<string, shared_ptr<Customer>> &customers; // & means refernce and means  us the original cuatomer database if no & wed get copy of customers like pass by val then cant modify  in main  
+    shared_ptr<Banker> banker; 
+    map<string, shared_ptr<Customer>> &customers; 
 public:
     BankerUI(shared_ptr<Banker> b, map<string, shared_ptr<Customer>> &custs);
-//     To create a BankerUI, I need
 
-// one banker
-// one customer database
+
+
+
     void displayMenu() override;
     void handleOperations() override;
 };
