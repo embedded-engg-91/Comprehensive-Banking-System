7833313c2b066c70b5359cd3ceecc2034190f17a
embedded-engg-91 <badigermanoj781@gmail.com>
2026-08-05 18:43:20 +0530

    Update insufficientFundsException.h

--- insufficientFundsException.h
+++ insufficientFundsException.h
@@ -6,7 +6,6 @@ using namespace std;
 class InsufficientFundsException : public BankingException
 {
 public:
-   // TODO: Write execetion function
+   
    InsufficientFundsException(): BankingException("Insufficient funds to complete this transaction."){}
 };
-//This constructor passes the error message to BankingException, so later e.what() will print it.
